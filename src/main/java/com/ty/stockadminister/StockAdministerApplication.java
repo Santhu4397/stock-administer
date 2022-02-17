afb0c8322b53f3e8e83e69190447283b7a8a2d06
Santhu4397 <santhoshdh129@gmail.com>
2022-02-17 13:26:28 +0530

    intial step

---
+++ src/main/java/com/ty/stockadminister/StockAdministerApplication.java
@@ -0,0 +1,13 @@
+package com.ty.stockadminister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAdministerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAdministerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/stockadminister/StockAdministerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.stockadminister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAdministerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
