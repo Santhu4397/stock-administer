1e7f176741affcda48d15f0ea46a12fabdb5fce8
Santhu4397 <santhoshdh129@gmail.com>
2022-02-18 11:49:29 +0530

    add a repository interface

--- src/main/java/com/ty/stockadminister/dto/SupplierDto.java
+++ src/main/java/com/ty/stockadminister/dto/SupplierDto.java
@@ -1,5 +1,50 @@
 package com.ty.stockadminister.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class SupplierDto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String mailId;
+	private long phone;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMailId() {
+		return mailId;
+	}
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 
 }
