f6668045057685b6312b70b1255d10325a6b5c1a
Santhu4397 <santhoshdh129@gmail.com>
2022-02-18 10:50:10 +0530

    suplier flow is done but no logic is written

---
+++ src/main/java/com/ty/stockadminister/service/SupplierService.java
@@ -0,0 +1,18 @@
+package com.ty.stockadminister.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+
+import com.ty.stockadminister.dto.SupplierDto;
+import com.ty.stockadminister.util.ResponseStructure;
+
+public interface SupplierService {
+	public ResponseEntity<ResponseStructure<SupplierDto>> save(SupplierDto dto);
+	public ResponseEntity<ResponseStructure<SupplierDto>>  getbyid(int id);
+	public ResponseEntity<ResponseStructure<List<SupplierDto>>>  getall();
+	public ResponseEntity<ResponseStructure<SupplierDto>> update(int id,SupplierDto supplierDto);
+	public ResponseEntity<ResponseStructure<String>> delete(int id);
+
+}
